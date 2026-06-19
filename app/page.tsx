9c0c7cad72f908ba7233fe60a8d160b23b0d7a3a
Raave Aires <developer@raave.me>
2026-06-19 16:29:01 -0300

    wip: inicia a estrutura da página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import { Background } from "@/components/aria/background";
+import { SettingsBallon } from "@/components/aria/settings-ballon";
 
 export default function Home() {
-  return <></>;
+  return (
+    <main className="relative min-w-dvw min-h-dvh overflow-hidden">
+      <Background />
+
+      <div className="absolute inset-0 z-0 bg-black/10" />
+      <section className="absolute z-10">
+        <div className="fixed top-4 right-4">
+          <SettingsBallon />
+        </div>
+      </section>
+    </main>
+  );
 }
